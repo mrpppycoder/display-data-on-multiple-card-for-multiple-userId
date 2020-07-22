37592f7193a27f13fba8638fd4c3a2a46d2ce287
prakash <3pattitude@gmail.com>
2020-07-22 07:11:13 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Data } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Cardtemp';
+  //title = 'image-gallery';
+  data:any = []
+ constructor(private http: HttpClient) {
 }
+getData(){
+ 
+  this.http.get('http://jsonplaceholder.typicode.com/todos').subscribe((res : Data[])=>{
+    res.forEach(element => {
+      console.log("record",element);
+    });
+    this.data = res
+    console.log("result",res);
+    console.log("data",this.data)
+  })
+}
+ngOnInit() {
+  this.getData()
+}
+}  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router'; // we also need angular router for Nebular to function properly
+import { NbButtonModule, NbListItemComponent, NbListModule } from '@nebular/theme';
+import {NbSidebarModule} from '@nebular/theme';
+import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent} from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule,NbCardModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule, // RouterModule.forRoot(routes, { useHash: true }), if this is your app.module
+    NbLayoutModule,
+    NbSidebarModule, // NbSidebarModule.forRoot(), //if this is your app.module
+    NbButtonModule,
     BrowserModule,
-    AppRoutingModule
+    NbListModule,
+    HttpClientModule,
+    AppRoutingModule,
+    NbSidebarModule.forRoot(), //<== add this,
+
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    //SideBarModule,
+    NbSidebarModule,
+    NbCardModule,
+
+    //<== add this
+   
+    NbEvaIconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
